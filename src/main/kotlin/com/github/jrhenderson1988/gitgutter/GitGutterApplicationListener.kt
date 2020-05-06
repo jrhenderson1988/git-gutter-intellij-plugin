ba55d15f2603bfac452a75703f78612482ae2ff4
Jonathon Henderson <jrhenderson1988@hotmail.com>
2020-05-06 23:58:11 +0100

    Improve method of hooking into gutter component to adapt its marker area width. Allows brace highlighting line position to stay consistent, doesn't change size of folding anchor buttons or distort anything

--- src/main/kotlin/com/github/jrhenderson1988/gitgutter/GitGutterApplicationListener.kt
+++ src/main/kotlin/com/github/jrhenderson1988/gitgutter/GitGutterApplicationListener.kt
@@ -2,16 +2,10 @@ package com.github.jrhenderson1988.gitgutter
 
 import com.intellij.ide.AppLifecycleListener
 import javassist.ClassPool
-import javassist.CtClass
-import javassist.CtField
-import javassist.Modifier
-import javassist.expr.ExprEditor
-import javassist.expr.FieldAccess
-import javassist.expr.MethodCall
 
 class GitGutterApplicationListener : AppLifecycleListener {
     companion object {
-        private const val multiplier = 0.5
+        private const val multiplier = 0.75
 
         init {
             hackGutter()
@@ -20,49 +14,22 @@ class GitGutterApplicationListener : AppLifecycleListener {
         private fun hackGutter() {
             try {
                 val cp = ClassPool(true)
+                val editorGutterComponent = cp.get("com.intellij.openapi.editor.impl.EditorGutterComponentImpl")
 
-                val braceHighlightingHandler = cp.get("com.intellij.codeInsight.highlighting.BraceHighlightingHandler")
-                val field = CtField(CtClass.intType, "lineMarkerOffset", braceHighlightingHandler)
-                field.modifiers = Modifier.PUBLIC or Modifier.STATIC
-                braceHighlightingHandler.addField(field, "0")
-                braceHighlightingHandler.toClass()
-
-                val lineStatusMarkerRenderer = cp.get("com.intellij.openapi.vcs.ex.LineStatusMarkerRenderer")
-                lineStatusMarkerRenderer
-                    .getDeclaredMethod("paintChangedLines")
-                    .instrument(object : ExprEditor() {
-                        override fun edit(m: MethodCall) {
-                            if (m.methodName == "getGutterArea") {
-                                m.replace(
-                                    """
-                                    {
-                                        com.intellij.diff.util.IntPair before = ${'$'}proceed(${"$$"});
-                                        int diff = before.val2 - before.val1;
-                                        double mul = 1.0d - ${multiplier}d;
-                                        int offs = (int) java.lang.Math.round(diff * mul);
-                                        com.intellij.codeInsight.highlighting.BraceHighlightingHandler.lineMarkerOffset = offs;
-                                        ${'$'}_ = new com.intellij.diff.util.IntPair(before.val1 + offs, before.val2);
-                                    }
-                                    """.trimIndent()
-                                )
-                            }
-                        }
-                    })
-                lineStatusMarkerRenderer.toClass()
-
-                val lineMarkerRenderer =
-                    cp.get("com.intellij.codeInsight.highlighting.BraceHighlightingHandler\$MyLineMarkerRenderer")
-
-                lineMarkerRenderer
-                    .getDeclaredMethod("paint")
-                    .instrument(object : ExprEditor() {
-                        override fun edit(f: FieldAccess) {
-                            if (f.fieldName == "x" && f.className == "java.awt.Rectangle") {
-                                f.replace("\$_ = \$proceed() + com.intellij.codeInsight.highlighting.BraceHighlightingHandler.lineMarkerOffset;")
-                            }
-                        }
-                    })
-                lineMarkerRenderer.toClass()
+                editorGutterComponent
+                    .apply {
+                        getDeclaredMethod("getLineMarkerFreePaintersAreaOffset")
+                            .setBody(
+                                """
+                                {
+                                    int start = getIconAreaOffset() + myIconsAreaWidth + getGapAfterIconsArea();
+                                    int end = getWhitespaceSeparatorOffset();
+                                    int offs = (int) Math.round((end - start) * (1.0d - ${multiplier}d));
+                                    return start + offs;
+                                }
+                                """.trimIndent()
+                            )
+                    }.toClass()
             } catch (e: Throwable) {
                 e.printStackTrace()
             }
