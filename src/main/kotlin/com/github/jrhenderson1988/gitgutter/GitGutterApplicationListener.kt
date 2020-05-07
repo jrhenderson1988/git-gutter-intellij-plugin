1e1ea8d54ab0129be6cca4d4be770ccbc952349f
Jonathon Henderson <jrhenderson1988@hotmail.com>
2020-05-08 00:40:39 +0100

    Refactor and use object instead of class with companion object

--- src/main/kotlin/com/github/jrhenderson1988/gitgutter/GitGutterApplicationListener.kt
+++ src/main/kotlin/com/github/jrhenderson1988/gitgutter/GitGutterApplicationListener.kt
@@ -3,36 +3,33 @@ package com.github.jrhenderson1988.gitgutter
 import com.intellij.ide.AppLifecycleListener
 import javassist.ClassPool
 
-class GitGutterApplicationListener : AppLifecycleListener {
-    companion object {
-        private const val multiplier = 0.75
+object GitGutterApplicationListener : AppLifecycleListener {
+    private const val multiplier = 0.75
 
-        init {
-            hackGutter()
-        }
+    init {
+        hackGutter()
+    }
 
-        private fun hackGutter() {
-            try {
-                val cp = ClassPool(true)
-                val editorGutterComponent = cp.get("com.intellij.openapi.editor.impl.EditorGutterComponentImpl")
+    private fun hackGutter() {
+        try {
+            val cp = ClassPool(true)
 
-                editorGutterComponent
-                    .apply {
-                        getDeclaredMethod("getLineMarkerFreePaintersAreaOffset")
-                            .setBody(
-                                """
-                                {
-                                    int start = getIconAreaOffset() + myIconsAreaWidth + getGapAfterIconsArea();
-                                    int end = getWhitespaceSeparatorOffset();
-                                    int offs = (int) Math.round((end - start) * (1.0d - ${multiplier}d));
-                                    return start + offs;
-                                }
-                                """.trimIndent()
-                            )
-                    }.toClass()
-            } catch (e: Throwable) {
-                e.printStackTrace()
-            }
+            cp.get("com.intellij.openapi.editor.impl.EditorGutterComponentImpl")
+                .apply {
+                    getDeclaredMethod("getLineMarkerFreePaintersAreaOffset")
+                        .setBody(
+                            """
+                            {
+                                int start = getIconAreaOffset() + myIconsAreaWidth + getGapAfterIconsArea();
+                                int end = getWhitespaceSeparatorOffset();
+                                int offs = (int) Math.round((end - start) * (1.0d - ${multiplier}d));
+                                return start + offs;
+                            }
+                            """.trimIndent()
+                        )
+                }.toClass()
+        } catch (e: Throwable) {
+            e.printStackTrace()
         }
     }
 }
