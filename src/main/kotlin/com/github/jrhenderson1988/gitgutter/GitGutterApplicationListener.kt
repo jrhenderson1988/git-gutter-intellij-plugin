47d7d4a255d64753976612dead62a0df48221c13
Jonathon Henderson <jrhenderson1988@hotmail.com>
2020-05-08 12:11:13 +0100

    Refactoring and implement applying alpha channel to gutter marker colours

--- src/main/kotlin/com/github/jrhenderson1988/gitgutter/GitGutterApplicationListener.kt
+++ src/main/kotlin/com/github/jrhenderson1988/gitgutter/GitGutterApplicationListener.kt
@@ -2,34 +2,85 @@ package com.github.jrhenderson1988.gitgutter
 
 import com.intellij.ide.AppLifecycleListener
 import javassist.ClassPool
+import javassist.CtNewMethod
+import javassist.expr.ExprEditor
+import javassist.expr.MethodCall
+import kotlin.math.roundToInt
 
 object GitGutterApplicationListener : AppLifecycleListener {
     private const val multiplier = 0.75
+    private const val alpha = 0.5
+    private val cp = ClassPool(true)
 
     init {
-        hackGutter()
-    }
-
-    private fun hackGutter() {
         try {
-            val cp = ClassPool(true)
-
-            cp.get("com.intellij.openapi.editor.impl.EditorGutterComponentImpl")
-                .apply {
-                    getDeclaredMethod("getLineMarkerFreePaintersAreaOffset")
-                        .setBody(
-                            """
-                            {
-                                int start = getIconAreaOffset() + myIconsAreaWidth + getGapAfterIconsArea();
-                                int end = getWhitespaceSeparatorOffset();
-                                int offs = (int) Math.round((end - start) * (1.0d - ${multiplier}d));
-                                return start + offs;
-                            }
-                            """.trimIndent()
-                        )
-                }.toClass()
+            resize().applyAlphaChannel()
         } catch (e: Throwable) {
             e.printStackTrace()
         }
+    }
+
+    private fun resize(): GitGutterApplicationListener {
+        cp.get("com.intellij.openapi.editor.impl.EditorGutterComponentImpl")
+            .apply {
+                getDeclaredMethod("getLineMarkerFreePaintersAreaOffset")
+                    .setBody(
+                        """
+                        {
+                            int start = getIconAreaOffset() + myIconsAreaWidth + getGapAfterIconsArea();
+                            int end = getWhitespaceSeparatorOffset();
+                            int offs = (int) Math.round((end - start) * (1.0d - ${multiplier}d));
+                            return start + offs;
+                        }
+                        """.trimIndent()
+                    )
+            }
+            .toClass()
+
+        return this
+    }
+
+    private fun applyAlphaChannel(): GitGutterApplicationListener {
+        val alpha = (alpha * 255).roundToInt().let {
+            when {
+                it < 0 -> 0
+                it > 255 -> 255
+                else -> it
+            }
+        }
+
+        println(alpha)
+        cp.get("com.intellij.openapi.vcs.ex.LineStatusMarkerRenderer")
+            .apply {
+                addMethod(
+                    CtNewMethod.make(
+                        """
+                        private static java.awt.Color applyAlphaToColor(java.awt.Color c) {
+                            return c == null ? null : new java.awt.Color(
+                                c.getRed(),
+                                c.getGreen(),
+                                c.getBlue(),
+                                $alpha
+                            );
+                        }
+                        """.trimIndent(), this
+                    )
+                )
+            }
+            .apply {
+                getDeclaredMethod("paintChangedLines")
+                    .instrument(object : ExprEditor() {
+                        override fun edit(m: MethodCall?) {
+                            when (m?.methodName) {
+                                "getGutterColor", "getGutterBorderColor", "getIgnoredGutterBorderColor" -> {
+                                    m.replace("{ \$_ = applyAlphaToColor(\$proceed(\$\$)); }")
+                                }
+                            }
+                        }
+                    })
+            }
+            .toClass()
+
+        return this
     }
 }
